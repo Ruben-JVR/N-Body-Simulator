ded63f16201f314452f0fbfd675e9c11847f56a8
KragKruger <117469835+Ruben-JVR@users.noreply.github.com>
2026-03-04 09:34:12 -0600

    Update n_body_problem.m

--- n_body_problem.m
+++ n_body_problem.m
@@ -20,6 +20,7 @@ function n_body_problem
 ##  strct1.timeSkip = 0.005;
 ##  strct1.selectedBody = 1;
 ##  strct1.displayPath = true;
+##  strct1.figTitle = 'Masses interacting only with gravitation';
 ##  %INITIAL CONDITION MODIFICATION
 ##
 ##  y1 = IConditions({'1 x 1','2 x -1','3 y 1', '3 z 1'}, strct1.N);
@@ -43,6 +44,7 @@ function n_body_problem
 ##  strct2.timeSkip = 0.005;
 ##  strct2.selectedBody = 1;
 ##  strct2.displayPath = true;
+##  strct2.figTitle = '4 masses interacting via gravitation';
 ##  y2 = IConditions({'1 x 1','2 y -1', '3 x -1', '4 y 1', '1 vy -1', '2 vx -1', '3 vy 1', '4 vx 1'}, strct2.N);
 ##
 ##  initialize_N_Body_Simulation(strct2,y2);
@@ -62,8 +64,9 @@ function n_body_problem
 ##  strct3.axs = [-1.5,1.5,-1.5,1.5,-1.5,1.5];
 ##  strct3.timePlaybackModifier = 2.5;
 ##  strct3.timeSkip = 0.01;
-##  strct3.selectedBody = nan;
+##  strct3.selectedBody = 1;
 ##  strct3.displayPath = true;
+##  strct3.figTitle = '3 masses interacting to create a figure eight';
 ##
 ##  y3 = IConditions({'1 x 0.9700436','1 y -0.24308753','2 x -0.9700436','2 y 0.24308753','1 vx 0.466203685','1 vy 0.43236573','2 vx 0.466203685','2 vy 0.43236573','3 vx -0.93240737','3 vy -0.86473146'}, strct3.N);
 ##
@@ -74,48 +77,72 @@ function n_body_problem
   %SIMULATION 4
   %CHARGED MASSES
   %==============================================
-##  strct4.figNum = 4;
-##  strct4.M = [1,1];
-##  strct4.Q = [1,-1];
-##  strct4.B = nan;
-##  strct4.N = size(strct4.M,2);
-##  strct4.FConstant = 1;
-##  strct4.QConstant = 1;
-##  strct4.integrationTime = [0,5];
-##  strct4.axs = [-1.5,1.5,-1.5,1.5,-1.5,1.5];
-##  strct4.timePlaybackModifier = 2.5;
-##  strct4.timeSkip = 0.01;
-##  strct4.selectedBody = nan;
-##  strct4.displayPath = true;
-##
-##  y4 = IConditions({'1 x -1','2 x 1','1 vy 1'}, strct4.N);
-##
-##  initialize_N_Body_Simulation(strct4,y4);
+  strct4.figNum = 4;
+  strct4.M = [1,1];
+  strct4.Q = [1,1];
+  strct4.B = nan;
+  strct4.N = size(strct4.M,2);
+  strct4.FConstant = 1;
+  strct4.QConstant = 1;
+  strct4.integrationTime = [0,5];
+  strct4.axs = [-1.5,1.5,-1.5,1.5,-1.5,1.5];
+  strct4.timePlaybackModifier = 2.5;
+  strct4.timeSkip = 0.01;
+  strct4.selectedBody = 1;
+  strct4.displayPath = true;
+  strct4.figTitle = '2 Bodies Interacting to Produce 0 Net Acceleration';
+
+  y4 = IConditions({'1 x -1','2 x 1','1 vy 0.5'}, strct4.N);
+
+  initialize_N_Body_Simulation(strct4,y4);
   %=============================================================
 
   %SIMULATION 5
   %CHARGED Particle in External Magnetic Field
   %============================================================
 
-  strct5.figNum = 5;
-  strct5.M = [1];
-  strct5.Q = [-1];
-  strct5.B = [0,0,1];
-  strct5.N = size(strct5.M,2);
-  strct5.FConstant = 1;
-  strct5.QConstant = 1;
-  strct5.integrationTime = [0,7];
-  strct5.axs = 4.*[-1.5,1.5,-1.5,1.5,-1.5,1.5];
-  strct5.timePlaybackModifier = 5;
-  strct5.timeSkip = 0.005;
-  strct5.selectedBody = nan;
-  strct5.displayPath = true;
-
-  y5 = IConditions({'1 x -1','1 vx 1'}, strct5.N);
-
-  initialize_N_Body_Simulation(strct5,y5);
+##  strct5.figNum = 5;
+##  strct5.M = [1];
+##  strct5.Q = [-1];
+##  strct5.B = [0,0,1];
+##  strct5.N = size(strct5.M,2);
+##  strct5.FConstant = 1;
+##  strct5.QConstant = 1;
+##  strct5.integrationTime = [0,7];
+##  strct5.axs = 4.*[-1.5,1.5,-1.5,1.5,-1.5,1.5];
+##  strct5.timePlaybackModifier = 5;
+##  strct5.timeSkip = 0.005;
+##  strct5.selectedBody = 1;
+##  strct5.displayPath = true;
+##  strct5.figTitle = 'Single charge under uniform magnetic field';
+##
+##  y5 = IConditions({'1 x 0','1 vx 1','1 y -3'}, strct5.N);
+##
+##  initialize_N_Body_Simulation(strct5,y5);
   %===========================================================
 
+
+  %SIMULATION 6
+  %===========================================================
+##  strct6.figNum = 6;
+##  strct6.M = [3,5,3,2];
+##  strct6.Q = [-3,1,-3,4];
+##  strct6.B = [0,0,1];
+##  strct6.N = size(strct6.M,2);
+##  strct6.FConstant = 1;
+##  strct6.QConstant = 1;
+##  strct6.integrationTime = [0,4];
+##  strct6.axs = 5.*[-1.5,1.5,-1.5,1.5,-1.5,1.5];
+##  strct6.timePlaybackModifier = 5;
+##  strct6.timeSkip = 0.005;
+##  strct6.selectedBody = 1;
+##  strct6.displayPath = true;
+##  strct6.figTitle = 'multiple charged masses under uniform magnetic field';
+##
+##  y6 = IConditions({'1 x -4','1 vx 1','1 y -3','3 y 3','4 x 4','2 y -3','3 vy -1','4 vx -2'}, strct6.N);
+##
+##  initialize_N_Body_Simulation(strct6,y6);
+  %=================================================================
 
 endfunction
 
@@ -192,6 +219,7 @@ function initialize_N_Body_Simulation(strct,Y)
   xlabel('x');
   ylabel('y');
   zlabel('z');
+  title(strct.figTitle);
 
   if strct.displayPath
     %Plot of gray orbit lines for total trajectory
