6f1eaf6ea4aa914501939be188448d207d7fcd82
KragKruger <117469835+Ruben-JVR@users.noreply.github.com>
2026-03-23 10:09:46 -0500

    Update derivs.m
    removed breakpoint markers

--- derivs.m
+++ derivs.m
@@ -1,5 +1,4 @@
 function dY = derivs(t,Y,q)
-  '';
   N = q.N;
   M = q.M;
   FConstant = q.FConstant;
@@ -26,16 +25,12 @@ function dY = derivs(t,Y,q)
   r = zeros(N-1,N-1);
   for i = 1:N
     for j = i+1:N
-
       r(i,j-1) = sqrt( (x(i) - x(j)).^2 + (y(i)-y(j)).^2 + (z(i) - z(j)).^2);
-
     endfor
   endfor
-
+  
   %loop through each body and calculate net acceleration in all three directions for each body
   %can do it more efficiently by doing both ax for i on j and j on i in the same step of the loop
-  '';
-
   for i = 1:(N-1)
 
     for j = i+1:N
@@ -48,10 +43,8 @@ function dY = derivs(t,Y,q)
       az(i) += ((FConstant .* M(j))./((r(i,j-1)).^3)) .* (z(j)-z(i));
       az(j) += ((FConstant .* M(i))./((r(i,j-1)).^3)) .* (z(i)-z(j));
     endfor
-
   endfor
 
-  '';
   dY(1:3:(3*N-2)) = vx;
   dY(2:3:(3*N-1)) = vy;
   dY(3:3:(3*N)) = vz;
@@ -60,6 +53,4 @@ function dY = derivs(t,Y,q)
   dY((3*N+2):3:6*N) = ay;
   dY((3*N+3):3:6*N) = az;
   dY = dY';
-  '';
-
 endfunction
